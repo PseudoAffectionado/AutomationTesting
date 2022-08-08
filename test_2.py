d9cf36317315b98e2e558d46f522cd0c15cc917a
PseudoAffectionado <bmitriebelonog@gmail.com>
2022-08-08 19:05:02 +0300

    Move all page logic to another file

--- test_2.py
+++ test_2.py
@@ -1,20 +1,14 @@
-from test_1 import *
+from page import *
 
 
 def test_empy_search(driver):
     click_search_form(driver)
-    type_text(driver, 'пупсичкbbbbbbbbbbbbbbbbbbи')
+    type_text(driver, 'qwertyadfgh')
     click_search_form(driver)
     click_search_button(driver)
     count_articles_number(driver)
 
     check_empty_page_text(driver)
-
-
-def check_empty_page_text(driver):
-    empty_res_locator = By.XPATH, '//*[@data-test-id="empty-placeholder-text"]'
-    empty_results = driver.find_element(*empty_res_locator)
-    print(f'Text on page: {empty_results.text}')
 
 
 if __name__ == '__main__':
