3299f0ddd5afd178da096ed13d8a00e7e81db631
PseudoAffectionado <bmitriebelonog@gmail.com>
2022-08-21 19:44:01 +0300

    Added base page object class

--- test_3.py
+++ test_3.py
@@ -4,5 +4,6 @@ from page import *
 def test_main_page(driver):
     page = MainPage(driver)
     page.open()
-    count_articles_number(driver)
-    count_pages_number(driver)
+
+    print(page.count_articles_number())
+    print(page.count_pages_number())
