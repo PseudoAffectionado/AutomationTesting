a5f45799f661d0b5311115629ade024017a804c9
PseudoAffectionado <bmitriebelonog@gmail.com>
2022-08-29 14:54:21 +0300

    Split tests to one assert — one test, change scope of driver's fixture

--- tests/search_tests/test_basic_search.py
+++ tests/search_tests/test_basic_search.py
@@ -1,10 +1,21 @@
 from page_objects.page import *
 
 
-def test_basic_search(driver):
+def test_articles_number_on_page(driver):
     page = MainPage(driver)
     page.open()
+
     page = page.click_search()
     page.search('Selenium')
+
     assert page.count_articles_number() == 20
+
+
+def test_pages_number_on_page(driver):
+    page = MainPage(driver)
+    page.open()
+
+    page = page.click_search()
+    page.search('Selenium')
+
     assert page.count_pages_number() == 50
--- tests/search_tests/test_empty_search.py
+++ tests/search_tests/test_empty_search.py
@@ -4,7 +4,18 @@ from page_objects.page import *
 def test_empy_search(driver):
     page = MainPage(driver)
     page.open()
+
     page = page.click_search()
     page.search('qwertyasdfgh')
+
     assert page.count_articles_number() == 0
+
+
+def test_empty_search_text(driver):
+    page = MainPage(driver)
+    page.open()
+
+    page = page.click_search()
+    page.search('qwertyasdfgh')
+
     assert len(page.get_empty_page_text()) > 0
