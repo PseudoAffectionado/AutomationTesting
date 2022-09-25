b3d1ed19c54cf7ed0ff101c9d326913c3a51b054
PseudoAffectionado <bmitriebelonog@gmail.com>
2022-09-25 18:58:38 +0300

    Fix multi tabs tests

--- tests/main_page_tests/conftest.py
+++ tests/main_page_tests/conftest.py
@@ -7,4 +7,11 @@ from page_objects.page import MainPage
 def page(driver):
     page = MainPage(driver)
     page.open()
-    return page
+    yield page
+
+    tabs = page.webdriver.window_handles
+    if len(tabs) > 1:
+        for tab in tabs[:0:-1]:
+            driver.switch_to.window(tab)
+            driver.close()
+    driver.switch_to.window(tabs[0])
